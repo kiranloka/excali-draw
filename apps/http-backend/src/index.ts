02bda8dd5172c845bfc10882ab3504893f05d98c
kiranLoka <kirankumarloka@gmail.com>
2025-03-29 17:24:49 +0530

    types and configurations

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,28 +1,49 @@
-import express,{Request,Response} from "express";
-import jwt from "jsonwebtoken";
-import {JWT_SECRET} from "./config";
-import { middleware } from "./middleware";
+import express, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '@repo/backend-common/config';
+import { middleware } from './middleware';
+import {
+  CreateUserSchema,
+  createRoomSchema,
+  SignInSchema,
+} from '@repo/common/types';
+
 const app = express();
 
-app.post("/room",middleware,(req:Request,res:Response)=>{
-   //db call
-   res.json({
-    message:"room Id"
-   }) 
+app.post('/room', middleware, (req: Request, res: Response) => {
+  const data = createRoomSchema.safeParse(req.body);
+  if (!data.success) {
+    res.status(400).json({
+      message: 'Incorrect inputs',
+    });
+    return;
+  }
+  //db call
+  res.json({
+    message: 'room Id',
+  });
 });
 
-app.post("/signup",(req:Request,res:Response)=>{
-//db call
-res.json({userId:"123"});
+app.post('/signup', (req: Request, res: Response) => {
+  const data = CreateUserSchema.safeParse(req.body);
+  if (!data.success) {
+    res.status(400).json({ message: 'Incorrect Inputs' });
+    return;
+  }
+  res.json({ userId: '123' });
+});
 
-})
+app.post('/signin', (req, res) => {
+  const data = SignInSchema.safeParse(req.body);
+  if (!data.success) {
+    res.status(400).json({ message: 'Incorrect Inputs' });
+    return;
+  }
+  const userId = 1;
+  const token = jwt.sign({ userId }, JWT_SECRET);
+  res.json({ token });
+});
 
-app.post("/signin",(req,res)=>{
-    const userId=1;
-    const token=jwt.sign({userId},JWT_SECRET)
-    res.json({token});
-})
-
-app.listen(3001,()=>{
-    console.log("listening on port 3001")
+app.listen(3001, () => {
+  console.log('listening on port 3001');
 });
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,25 +1,24 @@
-import { WebSocketServer } from "ws";
-import { JWT_SECRET } from "./config";
-import jwt, { JwtPayload } from "jsonwebtoken";
-const wss=new WebSocketServer({port:8080});
+import { WebSocketServer } from 'ws';
+import { JWT_SECRET } from '@repo/backend-common/config';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection',function connection(ws,request){
-    const url=request.url;
-    if(!url){
-        return;
-    }
+wss.on('connection', function connection(ws, request) {
+  const url = request.url;
+  if (!url) {
+    return;
+  }
 
+  const queryParams = new URLSearchParams(url.split('?')[1]);
+  const token = queryParams.get('token') || '';
+  const decoded = jwt.verify(token, JWT_SECRET);
 
-    const queryParams=new URLSearchParams(url.split('?')[1]);
-    const token=queryParams.get('token')||'';
-    const decoded=jwt.verify(token,JWT_SECRET);
-    
-    if(!decoded||!(decoded as JwtPayload).userId){
-        ws.close();
-        return;
-    }
-    
-    ws.on('message',function message(data){
-        ws.send('pong');
-    });
+  if (!decoded || !(decoded as JwtPayload).userId) {
+    ws.close();
+    return;
+  }
+
+  ws.on('message', function message(data) {
+    ws.send('pong');
+  });
 });
