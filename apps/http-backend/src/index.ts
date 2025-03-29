a7cb4254d08121dcf75785bc6161e86dc7b0d5d1
kiranLoka <kirankumarloka@gmail.com>
2025-03-29 16:29:37 +0530

    initial end point logic

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,27 @@
-import express from "express";
-
+import express,{Request,Response} from "express";
+import jwt from "jsonwebtoken";
+import {JWT_SECRET} from "./config";
+import { middleware } from "./middleware";
 const app = express();
+
+app.post("/room",middleware,(req:Request,res:Response)=>{
+   //db call
+   res.json({
+    message:"room Id"
+   }) 
+});
+
+app.post("/signup",(req:Request,res:Response)=>{
+//db call
+res.json({userId:"123"});
+
+})
+
+app.post("/signin",(req,res)=>{
+    const userId=1;
+    const token=jwt.sign({userId},JWT_SECRET)
+    res.json({token});
+})
 
 app.listen(3001,()=>{
     console.log("listening on port 3001")
